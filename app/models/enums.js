bb8ffac1410d355fcb551a2301754633c3b1225a
Riccardo D'Incau <52919690+RiccardoDincau@users.noreply.github.com>
2024-10-30 15:47:35 +0100

    Aggiunta raccolta di schemi enum

---
+++ app/models/enums.js
@@ -0,0 +1,6 @@
+module.exports = {
+    kind: ["report", "suggestion", "complaint"],
+    category: ["lights", "road", "trash"],
+    state: ["active", "work_in_progress", "archived"],
+    user_level: ["not_logged", "citizen", "admin"]
+};
