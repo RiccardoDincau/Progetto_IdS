2b90b5a552f29995c9a21240cccaac46abf6564c
Riccardo D'Incau <52919690+RiccardoDincau@users.noreply.github.com>
2024-11-03 21:06:22 +0100

    Creato schema report

--- app/models/report.js
+++ app/models/report.js
@@ -1,6 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var {kind, category, state} = require("./enums.js");
+
 module.exports = mongoose.model('Report', new Schema ({
-	_id: Schema.Types.ObjectId,
+	title: String,
+	content: String,
+	user: { type: Schema.Types.ObjectId, ref: 'User' },
+	votes: Number,
+	position: String,
+	kind, 
+	category, 
+	state,
+	comments: [ {type: Schema.Types.ObjectId, ref: "Comment"} ]
 }));
