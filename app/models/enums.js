9281f035478f71470244f774601a08d6a3fa5a59
Riccardo D'Incau <52919690+RiccardoDincau@users.noreply.github.com>
2024-11-03 19:46:20 +0100

    Corretta formattazione degli enum in Express

--- app/models/enums.js
+++ app/models/enums.js
@@ -1,6 +1,6 @@
 module.exports = {
-    kind: ["report", "suggestion", "complaint"],
-    category: ["lights", "road", "trash"],
-    state: ["active", "work_in_progress", "archived"],
-    user_level: ["not_logged", "citizen", "admin"]
+    kind: { type: String, enum: ["report", "suggestion", "complaint"] },
+    category: { type: String, enum: ["lights", "road", "trash"] },
+    state: { type: String, enum: ["active", "work_in_progress", "archived"] },
+    user_level: {type: String, enum: ["not_logged", "citizen", "admin"] }
 };
