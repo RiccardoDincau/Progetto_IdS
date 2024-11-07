4f1cd41c9233313ce93479ec62fadf02aeafe9ad
Tommaso Ascolani <tommaso_ascolani@LAPTOP-UKT26J0V>
2024-11-07 12:15:30 +0100

    Added post comment

--- app/reports.js
+++ app/reports.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const Report = require("./models/report.js");
 const User = require("./models/user.js");
+const Comment = require("./models/comment.js");
 const { user_level } = require("./models/enums.js");
+const user = require("./models/user.js");
 
 function displayedReport(mongooseReport) {
     return {
@@ -22,7 +24,7 @@ function displayedReport(mongooseReport) {
 
 function displayedComment(mongooseReport, mongooseComment) {
     return {
-        _id: "/report/"+mongooseReport._id,
+        _id: "/report/" + mongooseReport._id,
         _id: mongooseComment._id,
         content: mongooseComment.content,
         user: mongooseComment.user,
@@ -174,17 +176,41 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id/comments", async (req, res) => {
     let comments;
 
-    let report = await Report.findById(req.params.id).exec().catch(() => {
-        console.log("Error in Report quering (Id may be wrong)"); 
-    });
+    let report = await Report.findById(req.params.id)
+        .exec()
+        .catch(() => {
+            // find the report by the given id
+            console.log("Error in Report quering (Id may be wrong)");
+        });
 
-    comments = await report.comments.find({}).exec();
+    let sentQueries = {};
+    if (req.query.user) {
+        // req.query.user is in the format /user/:id
+        sentQueries["user"] = req.query.user;
 
-    comments = comments.map(displayedComment);
+        let userUrl = req.query.user;
+        let userID = userUrl.substring(userUrl.lastIndexOf("/") + 1); // extract the id from the /user/:id string
 
-    res.status(200).json(comments);
+        let user = null;
+        user = await User.findById(userID)
+            .exec()
+            .catch((err) => {
+                // find a user by his id
+                console.log("Error in user quering.\n", err);
+            });
 
-})
+        if (user == null) {
+            res.status(400).send("User does not exist\n");
+            return;
+        }
+    }
+
+    comments = await report.comments.find({ sentQueries }).exec(); // return an array of comments (mongoose format)
+
+    comments = comments.map(displayedComment); // displayComment on every element of the array comments
+
+    res.status(200).json(comments); // return the array (json format)
+});
 
 router.get("/:reportID/comments/:commentID", async (req, res) => {
     let report = await Report.findById(reportID)
@@ -209,7 +235,55 @@ router.get("/:reportID/comments/:commentID", async (req, res) => {
     res.status(200).json(displayedComment(report, comment));
 });
 
-router.delete("/:reportID/comments/:commentID", async (req, res) => {
+router.post("/report/:id/comment", async (req, res) => {
+    let requiredAttributes = ["content"];
+
+    let userUrl = req.body["user"];
+    let userID = userUrl.substring(userUrl.lastIndexOf("/") + 1);
+
+    let user = null;
+    user = await User.findById(userID)
+        .exec()
+        .catch((err) => {
+            console.log("Error in user quering.\n", err);
+        });
+
+    if (user == null) {
+        res.status(400).json({ error: "User does not exist" });
+        return;
+    }
+
+    let commentAttributes = {};
+    commentAttributes["user"] = userUrl;
+    for (let attr of requiredAttributes) {
+        if (req.body[attr] == undefined) {
+            console.log("Missing attribute: ", attr);
+            res.status(400).send("Missing attribute: " + attr);
+            return;
+        }
+
+        reportAttributes[attr] = req.body[attr];
+    }
+
+    let comment = new Comment(commentAttributes);
+
+    comment = await comment.save().catch((err) => {
+        console.log("Error in comment saving...");
+        res.status(404).send(
+            "Error in comment saving, probably an invalid enum was given"
+        );
+        return null;
+    });
+
+    if(comment == null) return;
+    // TODO
+    // da aggiungere il commento nello user
+
+    let commentID = comment.id;
+    res.location(req.path + commentID).status(201).send();
+});
+
+router.delete("/report/:reportID/comments/:commentID", async (req, res) => {
     let report = await Report.findById(req.params.reportID)
         .exec()
         .catch(() => {
