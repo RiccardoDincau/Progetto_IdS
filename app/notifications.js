c5311dc8d8a5a1b023b94a2afe4bf5a17a6ed527
Riccardo D'Incau <52919690+RiccardoDincau@users.noreply.github.com>
2024-11-16 12:35:01 +0100

    Aggiornata gestione errori notifications.js

--- app/notifications.js
+++ app/notifications.js
@@ -6,6 +6,7 @@ const User = require("./models/user.js");
 const Report = require("./models/report.js");
 const Comment = require("./models/comment.js");
 const tokenChecker = require("./tokenChecker.js");
+const errResp = require("./errors/errorResponse.js");
 
 function displayedNotification(mongooseNotification) {
     return {
@@ -24,21 +25,19 @@ function newUserDelete(oldUser, notifications) {
         email: oldUser.email,
         user_level: oldUser.user_level,
         reports: oldUser.reports,
-        notifications: notifications
+        notifications: notifications,
     };
 }
-function newUserAdd(oldUser, newNotifications){
+function newUserAdd(oldUser, newNotifications) {
     return {
         _id: mongooseUser._id,
         name: mongooseUser.name,
         email: mongooseUser.email,
         user_level: mongooseUser.user_level,
         reports: mongooseUser.reports,
-        notifications: newNotifications
+        notifications: newNotifications,
     };
 }
-
-
 
 //GET methods
 router.get("", async (req, res) => {
@@ -52,14 +51,10 @@ router.get("", async (req, res) => {
         if (body[el]) acceptedQueries[el] = body[el];
     }
 
-    let notification = await Notification.find(acceptedQueries)
-        .exec()
-        .catch((err) => {
-            res.status(400).send("Id format mey be wrong");
-        });
+    let notification = await Notification.find(acceptedQueries).exec();
 
     if (!notification) {
-        res.status(404).send("Notification not found");
+        errResp.notificationNotFound(res);
         return;
     }
     notification = notification.map(displayedNotification);
@@ -68,17 +63,12 @@ router.get("", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-    let interrupt = false;
     let notification = await Notification.findById(req.params.id)
         .exec()
-        .catch(() => {
-            console.log("Error in Notification quering (Id may be wrong)");
-            res.status(400);
-            interrupt = true;
-        });
+        .catch(() => errResp.idNotValid(res));
 
     if (!notification) {
-        if (!interrupt) res.status(404).send("Notification not found");
+        errResp.notificationNotFound(res);
         return;
     }
 
@@ -91,19 +81,17 @@ router.post("", async (req, res) => {
     let title = body.title;
     let content = body.content;
     let reportUrl = body.report;
-    let interrupt = false;
 
     let reportID = reportUrl.substring(reportUrl.lastIndexOf("/") + 1);
 
     let report = await Report.findById(reportID)
         .exec()
-        .catch((err) => {
-            res.status(400).send("Error in report quering");
-            interrupt = true;
-        });
+        .catch(() =>
+            errResp.idNotValid(res, { message: "Report id is not valid" })
+        );
 
     if (!report) {
-        if (!interrupt) res.status(404).send("Report not found");
+        errResp.reportNotFound(res);
         return;
     }
 
@@ -116,14 +104,14 @@ router.post("", async (req, res) => {
     notification = await notification.save();
 
     let notificationId = notification.id;
-    let comments = await Comments.find({report : reportID}).exec();
+    let comments = await Comment.find({ report: reportID }).exec();
 
-    let usersNewNotification=[];
-    for (let el of comments){
+    let usersNewNotification = [];
+    for (let el of comments) {
         usersNewNotification.push(el.user);
     }
 
-    for (let el of usersNewNotification){
+    for (let el of usersNewNotification) {
         let user = await User.findById(el).exec();
         let notifications = user.notification;
         notifications.push(notificationId);
@@ -138,21 +126,25 @@ router.post("", async (req, res) => {
 //DELETE methods
 router.delete("/:id", tokenChecker, async (req, res) => {
     let notificationID = req.params.id;
-    let userId=req.loggedUser.id;
+    let userId = req.loggedUser.id;
 
     //Search of the user whereby was sent the request
-    let user=await User.findById(userId).exec();
+    let user = await User.findById(userId).exec();
 
     let userNotification = user.notifications;
     let index = userNotification.indexOf(notificationID);
 
     //TODO: change the error response
-    if (index==-1)
-        res.status(404).send("Notification not found");
+    if (index == -1) {
+        errResp.notificationNotFound(res);
+    }
 
     userNotification.splice(index, 1);
 
-    User.findByIdAndUpdate(userId, newUserDelete(user, userNotification)).exec();
+    User.findByIdAndUpdate(
+        userId,
+        newUserDelete(user, userNotification)
+    ).exec();
 
     res.status(200).send();
 });
