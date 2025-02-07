59dfaca0e1fcda8e373c7d0cefe34de6e6b95646
Tommaso Ascolani <tommaso.ascolani04@gmail.com>
2025-02-07 09:57:50 +0100

    Aggiunto componente separato searchBarSCC.vue per la barra di ricerca

---
+++ vue-project/src/components/filterButtons/searchBarSCC.vue
@@ -0,0 +1,15 @@
+<template>
+    <input class="search-bar" type="text" placeholder="Cerca..." v-model="searchedValue" @input="emit('inFocus', searchedValue)">
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let searchedValue = ref("");
+let emit = defineEmits(['inFocus']);
+
+</script>
+
+<style>
+
+</style>
