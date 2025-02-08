2219a1f9f025f2697a8a835ab0c6a8e438e5a357
Riccardo D'Incau <52919690+RiccardoDincau@users.noreply.github.com>
2025-02-08 15:25:24 +0100

    Aggiunto GET /reports/:id/votes

--- app/votes.js
+++ app/votes.js
@@ -6,6 +6,31 @@ const tokenChecker = require("./tokenChecker.js");
 
 const errResp = require("./errors/errorResponse.js");
 
+router.get("/:id/votes", tokenChecker, async (req, res) => {
+    let report = await Report.findById(req.params.id)
+        .exec()
+        .catch(() => errResp.idNotValid(res));
+
+    if (!report) {
+        errResp.reportNotFound(res);
+        return;
+    }
+
+    let hasVoted = false;
+
+    let userUrl = req.loggedUser.id;
+    let userID = userUrl.substring(userUrl.lastIndexOf("/") + 1);
+
+    for (let user of report.votes) {
+        if (user == userID) {
+            hasVoted = true;
+            break;
+        }
+    }
+
+    res.status(200).json({ hasVoted });
+});
+
 router.put("/:id/votes", tokenChecker, async (req, res) => {
     let report = await Report.findById(req.params.id)
         .exec()
