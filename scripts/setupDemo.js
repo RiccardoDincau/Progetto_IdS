2bacdfa4b36b60067beba8a3492dd198ebcb9ce6
Riccardo D'Incau <52919690+RiccardoDincau@users.noreply.github.com>
2025-02-11 18:05:37 +0100

    Corretto script setupDemo

--- scripts/setupDemo.js
+++ scripts/setupDemo.js
@@ -15,9 +15,9 @@ mongoose
             make_user("riccardo", "riccardo@gmail.com", "riccardo1", "citizen"),
             make_user("marco", "marco@gmail.com", "marco1", "citizen"),
             make_user("tommaso", "tommaso@gmail.com", "tommaso1", "citizen"),
-            make_user("comune1", "comune1@gmail.com", "comune1", "citizen"),
-            make_user("comune2", "comune2@gmail.com", "comune2", "citizen"),
-            make_user("admin", "admin@gmail.com", "admin", "citizen"),
+            make_user("comune1", "comune1@gmail.com", "comune1", "district"),
+            make_user("comune2", "comune2@gmail.com", "comune2", "district"),
+            make_user("admin", "admin@gmail.com", "admin", "admin"),
         ];
 
         await User.deleteMany({}).then(async () => {
